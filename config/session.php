d6e0df9891cf631fc1cd72028a6887ad0c0b912a
Resty Gonzales <restyg931@gmail.com>
2026-08-15 09:35:19 +0800

    Update session.php

--- config/session.php
+++ config/session.php
@@ -5,5 +5,5 @@ use Framework\Config\Env;
 return [
     'driver'   => Env::get('SESSION_DRIVER', 'file'),
     'lifetime' => (int) Env::get('SESSION_LIFETIME', 120), // minutes
-    'secure'   => (bool) Env::get('SESSION_SECURE', false),
+    'secure'   => Env::get('SESSION_SECURE', null), // null (unset in .env) = auto-detect from request scheme
 ];
